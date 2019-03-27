7e783761c136e8e81013017e060ed890d84b3824
princeodd47 <princeodd@gmail.com>
2019-03-27 17:24:07 -0500

    bugfix(npc):npcs didn't set affiliation (#14)

--- squirrel_maze/resources/npc.py
+++ squirrel_maze/resources/npc.py
@@ -1,9 +1,11 @@
 from squirrel_maze.resources.actor import Actor
 
 
-def get_goblin(actor_id=0, name="Goblin"):
-    return Actor(actor_id=actor_id, pc_type="npc", name=name, level=1, max_hp=5, max_str=3, max_dex=2, max_sta=2)
+def get_goblin(actor_id=0, name='Goblin', pc_type='npc', affiliation='unfriendly'):
+    return Actor(actor_id=actor_id, pc_type=pc_type, affiliation=affiliation, name=name, level=1, max_hp=5, max_str=3,
+                 max_dex=2, max_sta=2)
 
 
-def get_big_goblin(actor_id=0, name="Big Goblin"):
-    return Actor(actor_id=actor_id, pc_type="npc", name=name, level=2, max_hp=10, max_str=9, max_dex=9, max_sta=9)
+def get_big_goblin(actor_id=0, name='Big Goblin', pc_type='npc', affiliation='unfriendly'):
+    return Actor(actor_id=actor_id, pc_type=pc_type, affiliation=affiliation, name=name, level=2, max_hp=10, max_str=9,
+                 max_dex=9, max_sta=9)
