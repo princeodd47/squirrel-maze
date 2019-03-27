3dc36ab46fa412a087f62e05d148de83afb93cf7
Steven Powell <princeodd@gmail.com>
2019-03-27 17:22:06 -0500

    bugfix(npc):npcs didn't set affiliation

--- test/unit/test_npc.py
+++ test/unit/test_npc.py
@@ -6,8 +6,18 @@ from squirrel_maze.resources import actor
 
 class TestNpc(unittest.TestCase):
 
-    def test_get_goblin(self):
+    def test_get_goblin_defaults(self):
         test_gob = npc.get_goblin()
 
-        self.assertTrue(isinstance(test_gob, actor.Actor))
-        self.assertTrue(test_gob.name == "Goblin")
+        assert isinstance(test_gob, actor.Actor)
+        assert test_gob.name == 'Goblin'
+        assert test_gob.pc_type == 'npc'
+        assert test_gob.affiliation == 'unfriendly'
+
+    def test_get_big_goblin_not_defaults(self):
+        test_gob = npc.get_big_goblin(name='Fooblin', pc_type='pc', affiliation='friendly')
+
+        assert isinstance(test_gob, actor.Actor)
+        assert test_gob.name == 'Fooblin'
+        assert test_gob.pc_type == 'pc'
+        assert test_gob.affiliation == 'friendly'
