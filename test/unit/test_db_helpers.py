29029a9649dcdac43978bde573d23b7a0a37913f
princeodd47 <princeodd@gmail.com>
2019-04-13 19:56:25 -0500

    add indexes to db (#27)

--- test/unit/test_db_helpers.py
+++ test/unit/test_db_helpers.py
@@ -6,7 +6,7 @@ from squirrel_maze.resources import db_helpers
 class TestDbHelper(unittest.TestCase):
     def test_get_actor(self):
         db = db_helpers.Database('test/unit/test_files/db.json')
-        actor = db.get_actor('pcs', 'Ham', affiliation='unfriendly')
+        actor = db.get_actor('pcs', 0, affiliation='unfriendly')
         assert actor.name == 'Ham'
         assert actor.max_hp == 10
         assert actor.pc_type == 'npc'
